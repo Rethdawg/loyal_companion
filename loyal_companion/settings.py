e4740ebaadbc6a32b5cc3589c33a11a6f181fc51
Robertas K <rethwall@gmail.com>
2024-01-17 13:49:50 +0200

    Currency Mogul App started.
    The Currency Mogul will be an app responsible for currency rates, currency conversions and historic currency rate graph creation via the Frankfurter API.

--- loyal_companion/settings.py
+++ loyal_companion/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'weatherman',
+    'currency_mogul',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- loyal_companion/urls.py
+++ loyal_companion/urls.py
@@ -22,4 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('weatherman/', include('weatherman.urls')),
+    path('currency/', include('currency_mogul.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
