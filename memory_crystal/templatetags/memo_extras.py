2dd7534509b82c158e0effc7a366f3067fd4f5ad
Robertas K <rethwall@gmail.com>
2024-01-21 18:15:34 +0200

    Memory crystal Inclusion Tag Offcanvas
    Off-canvas moved to a custom inclusion tag. Will contain categories and memos by month/year.

---
+++ memory_crystal/templatetags/memo_extras.py
@@ -0,0 +1,10 @@
+# Custom tags for use in the app
+from django import template
+from memory_crystal.models import Category
+register = template.Library()
+
+
+@register.inclusion_tag('memory_crystal/offcanvas.html')
+def render_offcanvas():
+    all_categories = Category.objects.all()
+    return {'all_categories': all_categories}
